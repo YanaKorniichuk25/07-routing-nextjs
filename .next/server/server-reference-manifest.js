4ddc7b31ca7f66fe9a9f355b5a40690517c60c3b
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2025-10-24 14:54:02 +0200

    Remove .next from repo

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WFgcrOusIy1F4YSmKUv9t9DxEDUZoz1Y/Qn6HqFCtu8=\"\n}"
--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_2e55dd12._.css",
-  "static/chunks/node_modules_b89ba9fb._.js",
-  "static/chunks/_0c2313a2._.js"
-],
-    source: "dynamic"
-});
